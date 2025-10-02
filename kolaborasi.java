6ea360f8a6ff831e1f74f2e4e01246accef32859
Weyznnn <rafaninoadiyatma0989@gmail.com>
2025-10-02 12:22:50 +0700

    Malas

--- kolaborasi.java
+++ kolaborasi.java
@@ -1,5 +1,8 @@
 public class kolaborasi {
     public static void main(String[] args) {
         System.out.println("Halo ini adalah projek kolaborasi");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Saya punya "+i+" nama");
+        }
     }
 }
